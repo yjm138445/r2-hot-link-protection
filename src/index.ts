9e39e32648e8dd4f0b19f1845d53408218dacfb7
Jin <mayuyangjiming@163.com>
2025-11-22 11:53:25 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 // 修改这个列表，更新为自己的 domain list
 const ALLOWED = new Set([
-  'app.justincourse.com',
-  'justincourse.com', 
-  'interjc.net'
+  'www.dm147.cc',
+  'play.dm147.cc'
 ]);
 const CORP    = 'same-site';     // same-origin 也行
 const BUCKET  = 'MEDIA';         // 对应 wrangler 的 r2_buckets 绑定名
