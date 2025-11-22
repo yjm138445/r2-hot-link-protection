686567e9eacf245f48ca96553932e7d1366b4e59
Jin <mayuyangjiming@163.com>
2025-11-22 12:04:11 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ export default {
 
     /* 4. 生成响应 + CORS/CORP 头 */
     const h = new Headers(obj.httpMetadata);
-    h.set('Cross-Origin-Resource-Policy', CORP); // 如果 worker 与目标域名在同一个根域名下，可以考虑打开
+    // h.set('Cross-Origin-Resource-Policy', CORP); // 如果 worker 与目标域名在同一个根域名下，可以考虑打开
     h.set('Access-Control-Allow-Origin',  refererOrigin);
     h.set('Vary',                         'Origin');          // 避免缓存污染
     h.set('Access-Control-Expose-Headers','Content-Length, Content-Range, Accept-Ranges');
